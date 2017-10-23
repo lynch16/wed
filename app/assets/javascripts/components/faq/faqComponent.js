927f12232fce527c4f1ce5af3a507e4c7b222efe
lynch16 <will.lynch91@gmail.com>
2017-10-22 21:07:26 -0400

    add'l faq

--- app/assets/javascripts/components/faq/faqComponent.js
+++ app/assets/javascripts/components/faq/faqComponent.js
@@ -12,6 +12,9 @@ function faqController() {
       question: "Will there be transporation after the reception?",
       answer: "Shuttles will be provided from the venue to the Hampton Inn and Suites following the Reception. Please note transportation to the event is not provided (carpool is recommended - this hotel is 2.4 mi from the venue so picking your car up the next day is easy!)"
   },{
+      question: "What should I wear?",
+      answer: "Formal or semi-formal attire - it's up to you! The Bride asks that you refrain from wearing a wedding dress. But whatever you decide, don't forget your dancing shoes!"
+  },{
       question: "How long does it take to get to La Piece?",
       answer: "Please allow at least 1 hour from Manchester, NH - Friday traffic can be crazy!!"
     },{
