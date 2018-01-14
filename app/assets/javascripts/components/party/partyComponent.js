e8bc49683e83a3dcdb55c87cf2d497068f7c2cc5
lynch16 <will.lynch91@gmail.com>
2018-01-14 13:46:59 -0500

    website updates

--- app/assets/javascripts/components/party/partyComponent.js
+++ app/assets/javascripts/components/party/partyComponent.js
@@ -7,38 +7,42 @@ app.component('partyComponent', {
 function partyController() {
   var partyCtrl = this;
   partyCtrl.$onInit = function() {
-    partyCtrl.maidOfHonor = {
+    partyCtrl.partyFilter = '';
+    partyCtrl.weddingParty = [{
       name: 'Ali Garner',
       imgURL: 'assets/196353_1944208885427_4307153_n.jpg',
-      description: "Sisters with the Bride since 1991.  Three words that sum up the bride's only sister - talented, intelligent, and opinionated.  Ali knows the words to every single Mariah Carey song ever written.  She is also an eyeball enthusiast (Optometrist) but will not be giving eye exams during the wedding."
-    };
-    partyCtrl.bestMan ={
+      description: "Sisters with the Bride since 1991.  Three words that sum up the bride's only sister - talented, intelligent, and opinionated.  Ali knows the words to every single Mariah Carey song ever written.  She is also an eyeball enthusiast (Optometrist) but will not be giving eye exams during the wedding.",
+      type: 'Maid of Honor'
+    },{
       name: 'Timothy Eagan',
       imgURL: 'assets/1926830_10152335310712105_1656148554059273514_n.jpg',
-      description: "1 of 3, no really, this guy is a triplet. The Groom has been a regular attendee of the Eagan family Easter Dinner for 4 years running. Tim is also the love of Daisy's life. He has a love/hate relationship with Gin."
-    };
-    partyCtrl.bridesmaids = [{
+      description: "1 of 3, no really, this guy is a triplet. The Groom has been a regular attendee of the Eagan family Easter Dinner for 4 years running. Tim is also the love of Daisy's life. He has a love/hate relationship with Gin.",
+      type: 'Best Man'
+    },{
       name: 'Sara Bee',
       imgURL: 'assets/1383145_10200932520618429_838699888_n.jpg',
-      description: "Became friends with the Bride during a Mariah Carey concert when they were 12 years young. Sara is always on the go. When she's not skiiing, boating, hiking, attending a music festival, or traveling to Italy with the Bride, she finds time for dedicating Karaoke songs to her boyfriend of 14 years."
-    }, {
-      name: 'Tara McLaughlin',
-      imgURL: 'assets/19656991_10213665427535804_5521766038478399178_n.jpg',
-      description: "Currently residing in Los Angeles, Tara is a night owl who spends her nights either in pajamas reading until 4am or attending the Golden Globes. Initially bonded with the Bride during an AP Biology summer project, they remain long distance pals through their mutual love of Papa Johns pizza and secretly rummaging their parents' liquor cabinets."
-    }, {
-      name: 'Julia Heald',
-      imgURL: 'assets/10173583_10201653556203790_1045325341_n.jpg',
-      description: "Best friends with a dolphin (yes, really). Julia is a Marine Mammal Trainer in Clearwater, Florida and is by far the Bride's bubbliest friend. Smiling is definitely her favorite."
-    }];
-
-    partyCtrl.groomsmen = [{
+      description: "Became friends with the Bride during a Mariah Carey concert when they were 12 years young. Sara is always on the go. When she's not skiiing, boating, hiking, attending a music festival, or traveling to Italy with the Bride, she finds time for dedicating Karaoke songs to her boyfriend of 14 years.",
+      type: 'Bridesmaid'
+    },  {
       name: 'Connor Scofield',
       imgURL: 'assets/12525162_1750647025167196_2744116062943660081_o.jpg',
-      description: "Befriended the Groom via Cragislist circa 2010. Connor ('Sco') and the groom were roommates at UNH, even after their bachelor pad burned down (yup, that really happened). Sco is a film producer currently living in Los Angeles. If you're looking to talk to this fun-loving guy at the wedding, no doubt he'll be located either at the bar or on the dance floor."
+      description: "Befriended the Groom via Cragislist circa 2010. Connor ('Sco') and the groom were roommates at UNH, even after their bachelor pad burned down (yup, that really happened). Sco is a film producer currently living in Los Angeles. If you're looking to talk to this fun-loving guy at the wedding, no doubt he'll be located either at the bar or on the dance floor.",
+      type: 'Groomsman'
+    },{
+      name: 'Tara McLaughlin',
+      imgURL: 'assets/19656991_10213665427535804_5521766038478399178_n.jpg',
+      description: "Currently residing in Los Angeles, Tara is a night owl who spends her nights either in pajamas reading until 4am or attending the Golden Globes. Initially bonded with the Bride during an AP Biology summer project, they remain long distance pals through their mutual love of Papa Johns pizza and secretly rummaging their parents' liquor cabinets.",
+      type: 'Bridesmaid'
     }, {
       name: 'Greg Bennet',
       imgURL: 'assets/482347_10151574413961468_260726141_n.jpg',
-      description: "A Maine resident with a green thumb. This laid back guy loves music, bonfires with good friends, his girlfriend, Molly, and their 2 dogs."
-    }];
+      description: "A Maine resident with a green thumb. This laid back guy loves music, bonfires with good friends, his girlfriend, Molly, and their 2 dogs.",
+      type: 'Groomsman'
+  },{
+      name: 'Julia Heald',
+      imgURL: 'assets/10173583_10201653556203790_1045325341_n.jpg',
+      description: "Best friends with a dolphin (yes, really). Julia is a Marine Mammal Trainer in Clearwater, Florida and is by far the Bride's bubbliest friend. Smiling is definitely her favorite.",
+      type: 'Bridesmaid'
+  }];
   };
 }
--- app/assets/javascripts/components/wedding/weddingComponent.js
+++ app/assets/javascripts/components/wedding/weddingComponent.js
@@ -1,30 +1,139 @@
 app.component('weddingComponent', {
   templateUrl: 'components/wedding/_wedding.html',
-  controller: ["NgMap", weddingController],
+  controller: ["NgMap", '$filter', weddingController],
   controllerAs: "weddingCtrl"
 });
 
-function weddingController(NgMap) {
+function weddingController(NgMap, $filter) {
   var weddingCtrl = this;
   weddingCtrl.$onInit = function() {
     var stayIcon = 'http://maps.google.com/mapfiles/ms/icons/green-dot.png';
     var activityIcon = 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png';
+    var barIcon = 'http://maps.google.com/mapfiles/ms/icons/orange-dot.png';
     weddingCtrl.places = [
         {
+            id: 1,
             title: 'La Piece',
             position: '43.443193,-71.5934472',
+            link: 'http://lapiece-theroom.com',
+            type: 'Wedding Venue',
             centered: true
         }, {
+            id: 2,
             title: 'Hampton Inn and Suites',
             position: '43.459487,-71.554438',
             icon: stayIcon,
+            link: 'http://hamptoninn3.hilton.com/en/hotels/new-hampshire/hampton-inn-and-suites-tilton-CONTNHX/index.html',
+            type: 'Accomodation',
             centered: false
         },{
+            id: 3,
             title: 'Lake Opechee Inn and Spa',
             position: '43.545707,-71.4676527',
             icon: stayIcon,
+            link: 'http://www.opecheeinn.com',
+            type: 'Accomodation',
+            centered: false
+        },{
+            id: 4,
+            title: "Tilt'n Diner",
+            position: '43.4551086,-71.5684757',
+            icon: barIcon,
+            link: 'http://www.thecman.com/restaurants-and-menus/tilt-n-diner.aspx',
+            type: 'Restaurant',
+            centered: false
+        },{
+            id: 5,
+            title: "The Tilton Inn",
+            position: '43.4424179,-71.5905597',
+            icon: barIcon,
+            link: 'https://www.thetiltoninn.com',
+            type: 'Restaurant',
+            centered: false
+        },{
+            id: 6,
+            title: "The Barley House",
+            position: '43.2075792,-71.5385574',
+            icon: barIcon,
+            link: 'http://concord.thebarleyhouse.com',
+            type: 'Restaurant',
+            centered: false
+        },{
+            id: 7,
+            title: "The Common Man",
+            position: '43.2076868,-71.6064085',
+            icon: barIcon,
+            link: 'http://www.thecman.com/restaurants-and-menus/common-man-restaurants/common-man-concord.aspx',
+            type: 'Restaurant',
+            centered: false
+        },{
+            id: 8,
+            title: "Lago",
+            position: '43.658111,-71.5012073',
+            icon: barIcon,
+            link: 'http://www.thecman.com/restaurants-and-menus/lago.aspx',
+            type: 'Restaurant',
+            centered: false
+        },{
+            id: 9,
+            title: "Town Docks",
+            position: '43.6550489,-71.4994487',
+            icon: barIcon,
+            link: 'http://www.thecman.com/restaurants-and-menus/town-docks.aspx',
+            type: 'Restaurant',
+            centered: false
+        },{
+            id: 10,
+            title: "Weirs Beach - Lake Winnipesaukee",
+            position: '43.6089939,-71.4781384',
+            icon: activityIcon,
+            link: 'http://www.lakewinnipesaukee.info/things-to-do',
+            type: 'Activity',
+            centered: false
+        },{
+            id: 11,
+            title: "Bartlett Beach - Lake Winnisquam",
+            position: '43.5257172,-71.5112133',
+            icon: activityIcon,
+            link: 'https://blog.nhstateparks.org/mt-major-family-friendly-hike',
+            type: 'Activity',
+            centered: false
+        },{
+            id: 12,
+            title: "Pitmans Freight Room",
+            position: '43.5272073,-71.475865',
+            icon: activityIcon,
+            link: 'http://pitmansfreightroom.com',
+            type: 'Activity',
+            centered: false
+        },{
+            id: 13,
+            title: "Tanger Outlets",
+            position: '43.4594829,-71.5619187',
+            icon: activityIcon,
+            link: 'https://www.tangeroutlet.com/tilton/stores',
+            type: 'Activity',
+            centered: false
+        },{
+            id: 14,
+            title: "Just Maple",
+            position: '43.4734576,-71.5907765',
+            icon: activityIcon,
+            link: 'http://www.justmaple.com',
+            type: 'Activity',
             centered: false
         }];
-    NgMap.getMap();
+    NgMap.getMap().then(function (map) {
+        weddingCtrl.map = map;
+    });
+    weddingCtrl.mapCenter = $filter('filter')(weddingCtrl.places, {centered: true}, true)[0].position;
   };
+  weddingCtrl.openWindow = function (event, place) {
+      weddingCtrl.activeMarker = place;
+      weddingCtrl.mapCenter = place.position;
+      weddingCtrl.map.showInfoWindow('details', place);
+  };
+  weddingCtrl.hideWindow = function() {
+   weddingCtrl.map.hideInfoWindow('details');
+ };
 }
