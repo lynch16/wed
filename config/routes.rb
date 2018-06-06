5eb01a00a6927a76223573d202f52ddd0a4c74c4
Will Lynch <will@awato.org>
2018-06-06 13:26:51 -0400

    fix www redirect

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
+correct_url = "http://www.letsbelynches.com"
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  constraints(:host => /letsbelynches.com/) do
+    root to: redirect("#{correct_url}")
+    get '*/path' => redirect {|p| "#{correct_url}/#{p[:path]}"}
+  end
   scope :api do
       resources :contact, only: [:create]
   end
