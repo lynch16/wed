aa9cdc7d04a6023290aa80fae853a7b35906118c
Will Lynch <will.lynch91@gmail.com>
2018-06-06 14:07:58 -0400

    try with match

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ correct_url = "http://www.letsbelynches.com"
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   constraints(host: /^(?!www\.)/i) do
-    get '(*any)' => redirect { |params, request|
-      URI.parse(request.url).tap { |uri| uri.host = "www.#{uri.host}" }.to_s
-    }
+#     get '(*any)' => redirect { |params, request|
+#       URI.parse(request.url).tap { |uri| uri.host = "www.#{uri.host}" }.to_s
+#     }
+    match "/(*path)" => redirect {|params, request| 
+      URI.parse(request.url).tap { |uri| uri.host = "www.#{uri.host}/#{params[:path]}" }.to_s
+    },  via: [:get, :post]
   end
   scope :api do
       resources :contact, only: [:create]
